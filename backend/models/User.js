86112bf2cdbfbaeb0cd02f3a00472a1fefffb047
harini <harinisunkara2005@gmail.com>
2025-11-12 11:09:10 +0530

    Fix: Update User model and gitignore

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,40 @@
+// backend/models/User.js
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
+// Define User schema
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
     unique: true,
     lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
   },
 });
 
-// Hash password before saving
+// Pre-save hook to hash password
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  try {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
+// Method to compare password for login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Export the model
 module.exports = mongoose.model("User", userSchema);
